86f30016f09dee4ec96e4805aa969b0e464acce2
Maxik <mpoplavskiy38380@gmail.com>
2023-11-17 22:04:03 +0200

    Add balls movement

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function animation() {
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+async function animation() {
     const areaHeight = document.getElementById("anim").offsetHeight;
-    
     const areaWidth = document.getElementById("anim").offsetWidth;
-    const orangeBall = {y:areaHeight-25,x:Math.max(5, Math.min(areaWidth-25, Math.random()*areaWidth))};
-    const blueBall = {y:5,x:Math.max(5, Math.min(areaWidth-25, Math.random()*areaWidth))};
+    
+    let orangeBallYSpeed = -Math.max(0.1, Math.random());
+    const orangeBall = {y:areaHeight-25,
+        x:Math.max(5, Math.min(areaWidth-25, Math.random()*areaWidth)),
+        ySpeed: orangeBallYSpeed,
+        xSpeed: (Math.round(Math.random()) * 2 - 1) * (1 - orangeBallYSpeed**2)
+    };
+    
+    let blueBallYSpeed = Math.max(0.1, Math.random());
+    const blueBall = {y:5,
+        x:Math.max(5, Math.min(areaWidth-25, Math.random()*areaWidth)),
+        ySpeed: blueBallYSpeed,
+        xSpeed: (Math.round(Math.random()) * 2 - 1) * (1 - blueBallYSpeed**2)
+    };
     
     const orangeBallElement = document.getElementById("orange-ball");
     const blueBallElement = document.getElementById("blue-ball");
@@ -15,6 +28,28 @@ function animation() {
     
     blueBallElement.style.bottom = blueBall.y + "px";
     blueBallElement.style.left = blueBall.x + "px";
+
+    while(true) {
+        await sleep(10);
+
+        orangeBall.ySpeed = 5 > orangeBall.y || orangeBall.y > areaHeight-25 ? -orangeBall.ySpeed : orangeBall.ySpeed;
+        orangeBall.y += orangeBall.ySpeed;
+
+        orangeBall.xSpeed = 5 > orangeBall.x || orangeBall.x > areaWidth-25 ? -orangeBall.xSpeed : orangeBall.xSpeed;
+        orangeBall.x += orangeBall.xSpeed;
+
+        blueBall.ySpeed = 5 > blueBall.y || blueBall.y > areaHeight-25 ? -blueBall.ySpeed : blueBall.ySpeed;
+        blueBall.y += blueBall.ySpeed;
+
+        blueBall.xSpeed = 5 > blueBall.x || blueBall.x > areaWidth-25 ? -blueBall.xSpeed : blueBall.xSpeed;
+        blueBall.x += blueBall.xSpeed;
+
+        orangeBallElement.style.bottom = orangeBall.y + "px";
+        orangeBallElement.style.left = orangeBall.x + "px";
+        
+        blueBallElement.style.bottom = blueBall.y + "px";
+        blueBallElement.style.left = blueBall.x + "px";
+    }
 }
 
 function toggleWorkShow() {
