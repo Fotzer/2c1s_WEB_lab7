96f1cddf7d1f41581cd13168086f2d8cfad9986a
Maxik <mpoplavskiy38380@gmail.com>
2023-11-18 19:32:45 +0200

    Implement events and events log to localStorage and server

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ async function animation() {
 
         if(orangeBall.y > areaHeight/2 && blueBall.y > areaHeight/2 || orangeBall.y+20 < areaHeight/2 && blueBall.y+20 < areaHeight/2) {
             showReload();
+            if(orangeBall.y > areaHeight/2 && blueBall.y > areaHeight/2) {
+                window.dispatchEvent(new CustomEvent("balls-in-upper-half"));
+            }
+            if(orangeBall.y+20 < areaHeight/2 && blueBall.y+20 < areaHeight/2) {
+                window.dispatchEvent(new CustomEvent("balls-in-lower-half"));
+            }
             break;
         }
 
@@ -62,6 +68,8 @@ async function animation() {
 
             blueBall.ySpeed = -blueBall.ySpeed;
             blueBall.xSpeed = -blueBall.xSpeed;
+
+            window.dispatchEvent(new CustomEvent("balls-collision"));
         }
 
         orangeBall.ySpeed = 5 > orangeBall.y || orangeBall.y > areaHeight-25 ? -orangeBall.ySpeed : orangeBall.ySpeed;
@@ -72,6 +80,8 @@ async function animation() {
         orangeBall.x = Math.max(5, Math.min(areaWidth-25, orangeBall.x));
         orangeBall.x += orangeBall.xSpeed;
 
+        window.dispatchEvent(new CustomEvent("orange-ball-move"));
+
         blueBall.ySpeed = 5 > blueBall.y || blueBall.y > areaHeight-25 ? -blueBall.ySpeed : blueBall.ySpeed;
         blueBall.y = Math.max(5, Math.min(areaHeight-25, blueBall.y));
         blueBall.y += blueBall.ySpeed;
@@ -79,6 +89,8 @@ async function animation() {
         blueBall.xSpeed = 5 > blueBall.x || blueBall.x > areaWidth-25 ? -blueBall.xSpeed : blueBall.xSpeed;
         blueBall.x = Math.max(5, Math.min(areaWidth-25, blueBall.x));
         blueBall.x += blueBall.xSpeed;
+
+        window.dispatchEvent(new CustomEvent("blue-ball-move"));
 
         orangeBallElement.style.bottom = orangeBall.y + "px";
         orangeBallElement.style.left = orangeBall.x + "px";
@@ -94,6 +106,8 @@ function reload() {
     document.getElementById("controls-start-button").classList.remove("hidden");
 
     randomBallsPosition();
+
+    window.dispatchEvent(new CustomEvent("reload-click"));
 }
 
 document.getElementById("controls-reload-button").onclick = reload;
@@ -101,6 +115,7 @@ document.getElementById("controls-reload-button").onclick = reload;
 document.getElementById("controls-start-button").addEventListener("click", animation);
 document.getElementById("controls-start-button").addEventListener("click", () => {
     document.getElementById("controls-start-button").classList.add("disabled");
+    window.dispatchEvent(new CustomEvent("start-click"));
 });
 
 function toggleWorkShow() {
@@ -110,5 +125,58 @@ function toggleWorkShow() {
 
 document.getElementById("show-window-button").addEventListener("click", toggleWorkShow);
 document.getElementById("show-window-button").addEventListener("click", randomBallsPosition);
+document.getElementById("show-window-button").addEventListener("click", () => {
+    window.dispatchEvent(new CustomEvent("play-click"));
+});
 
-document.getElementById("controls-close-button").onclick = toggleWorkShow;
+document.getElementById("controls-close-button").addEventListener("click", toggleWorkShow);
+document.getElementById("controls-close-button").addEventListener("click", () => {
+    window.dispatchEvent(new CustomEvent("close-click"));
+});
+
+async function sendEventOnServer(event, eventNum) {
+    const formData = new FormData();
+    formData.append("data", `${eventNum} ${event}`);
+    const response = await fetch("/writeEvent.php", {method:"POST",
+        body:formData
+    });
+    console.log(response);
+}
+
+async function writeEventToLocalStorage(event, eventNum) {
+    const data = JSON.parse(localStorage.getItem("data"));
+    data.data.push(eventNum, event, new Date().toLocaleTimeString());
+    localStorage.setItem("data", JSON.stringify(data));
+}
+
+window.addEventListener("load", () => {
+    const data = {data:[]};
+    localStorage.setItem("data", JSON.stringify(data));
+});
+
+let eventNum = 1;
+async function logEvent(event) {
+    const type = event.type;
+    sendEventOnServer(type, eventNum);
+    writeEventToLocalStorage(type, eventNum);
+
+    displayEvent(type, eventNum);
+
+    eventNum++;
+}
+
+window.addEventListener("wall-collision", logEvent);
+window.addEventListener("balls-collision", logEvent);
+window.addEventListener("blue-ball-move", logEvent);
+window.addEventListener("orange-ball-move", logEvent);
+window.addEventListener("play-click", logEvent);
+window.addEventListener("start-click", logEvent);
+window.addEventListener("reload-click", logEvent);
+window.addEventListener("close-click", logEvent);
+window.addEventListener("balls-in-upper-half", logEvent);
+window.addEventListener("balls-in-lower-half", logEvent);
+
+function displayEvent(event, eventNum) {
+    const eventDisplayer = document.getElementById("controls-event-display");
+    eventDisplayer.innerText = `${eventNum} ${event}`
+}
---
+++ writeEvent.php
@@ -0,0 +1,14 @@
+<?php
+if(file_exists('log')) 
+{
+    $fp = fopen('log', 'a');
+    fwrite($fp, "\n".$_POST['data'].' '.date("H:i:s"));  
+    fclose($fp);  
+}
+else
+{
+    $fp = fopen('log', 'w');
+    fwrite($fp, $_POST['data']);  
+    fclose($fp);
+}
+?> 
