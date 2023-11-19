cca905cdb3ae03f7570185a8d127213a9724df63
Maxik <mpoplavskiy38380@gmail.com>
2023-11-19 10:50:25 +0200

    Fix timespatmp on server

--- writeEvent.php
+++ writeEvent.php
@@ -2,13 +2,13 @@
 if(file_exists('log')) 
 {
     $fp = fopen('log', 'a');
-    fwrite($fp, "\n".$_POST['data'].' '.date("H:i:s"));  
+    fwrite($fp, "\n".$_POST['data'].' '.date("H:i:s", strtotime('+2 hours', $now)));
     fclose($fp);  
 }
 else
 {
     $fp = fopen('log', 'w');
-    fwrite($fp, $_POST['data'].' '.date("H:i:s"));  
+    fwrite($fp, $_POST['data'].' '.date("H:i:s", strtotime('+2 hours', $now)));  
     fclose($fp);
 }
 ?> 
